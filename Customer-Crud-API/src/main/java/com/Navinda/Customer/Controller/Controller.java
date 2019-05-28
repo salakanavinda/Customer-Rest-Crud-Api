f668edbca8613681343835f602c32f189c90dedc
Salaka Navinda <mmsnavinda@gmail.com>
2019-05-28 20:29:19 +0530

    I add fetch all customers and fetch customer for specific id
    Add two types of end points

--- Customer-Crud-API/src/main/java/com/Navinda/Customer/Controller/Controller.java
+++ Customer-Crud-API/src/main/java/com/Navinda/Customer/Controller/Controller.java
@@ -1,6 +1,10 @@
 package com.Navinda.Customer.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.Navinda.Customer.Model.Customer;
 import com.Navinda.Customer.Service.CustomerService;
+
 
 @RestController
 @RequestMapping(value="/cus-cloud")
@@ -20,4 +25,25 @@ public class Controller {
 	public Customer saveCustomer(@RequestBody Customer customer) {
 		return customrService.save(customer);
 	}
+	
+	@RequestMapping(value="/customer", method=RequestMethod.GET)
+	public List<Customer> getAll(){
+		return customrService.fetch();
+	}
+	
+	@RequestMapping(value="/customer/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Customer> findById(@PathVariable Integer id){
+		if(id<=0) {
+			return ResponseEntity.badRequest().build();
+		}else {
+			Customer customer = customrService.fetch(id);
+			if(customer==null) {
+				return ResponseEntity.notFound().build();
+			}else {
+				return ResponseEntity.ok(customer);
+			}
+		}
+	}
+	
 }
+
