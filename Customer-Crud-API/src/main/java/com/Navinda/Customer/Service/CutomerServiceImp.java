f6089cc7aedc6d30c91758a87b71d2e60986c855
Nilaksha <nilaksha.dila@gmail.com>
2019-05-29 10:03:51 +0530

    modified pom and add service annotation to the customerServiceIml

--- Customer-Crud-API/src/main/java/com/Navinda/Customer/Service/CutomerServiceImp.java
+++ Customer-Crud-API/src/main/java/com/Navinda/Customer/Service/CutomerServiceImp.java
@@ -3,11 +3,12 @@ package com.Navinda.Customer.Service;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.Navinda.Customer.Model.Customer;
 import com.Navinda.Customer.Repository.CustomerRepository;
 
-
+@Service
 public class CutomerServiceImp implements CustomerService {
 
 	@Autowired
